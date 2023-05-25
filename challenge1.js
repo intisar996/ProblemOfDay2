2178ae5d5c0341df6459795d65d6d9b2b7df3645
intisar said <107991431+intisar996@users.noreply.github.com>
2023-05-25 12:05:16 +0400

    Add files via upload

---
+++ challenge1.js
@@ -0,0 +1,22 @@
+
+var a = document.getElementById("a");
+var b = document.getElementById("b");
+var c = document.getElementById("c");
+
+ a = prompt("Enter First Number");
+ b = prompt("Enter Second Number");
+ c = prompt("Enter  Thread Number");
+
+
+
+ if(a == b == c) {
+    alert("Number IS equal")
+ }else {
+    alert("Number IS large  " +Math.max(a ,b,c))
+
+ }
+
+
+
+
+
